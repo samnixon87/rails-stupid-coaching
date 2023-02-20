6c6cf58260c95f10cd6892dffdd4f289e32bb11c
Sam Nixon samnixon87@gmail.com <samnixon87@gmail.com>
2023-02-20 13:59:07 +0000

    Finished second challenge

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,19 @@
+class QuestionsController < ApplicationController
+
+  def ask
+  end
+
+  def answer
+
+    @question = params[:question]
+    @answer = nil
+
+    if @question == "I am going to work"
+      @answer = "Great!"
+    elsif @question.end_with?("?")
+      @answer = "Silly question, get dressed and go to work!"
+    else
+      @answer = "I don't care, get dressed and go to work!"
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,10 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  # Ask route
+  get '/ask', to: 'questions#ask'
+
+  # Answer route
+  get '/answer', to: 'questions#answer'
 end
